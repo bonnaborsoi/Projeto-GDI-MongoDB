b82096e50384e208e62deceef691c7b83f9bf3f1
Bonna Borsoi <86304672+bonnaborsoi@users.noreply.github.com>
2022-10-10 20:43:16 -0300

    Update povoamento.js

--- povoamento.js
+++ povoamento.js
@@ -354,3 +354,333 @@ db.visual_media.insertMany([
   "metacritic_must_see": false,
   "tipo": "série"
 }]);
+
+// Criando a coleção de streamings
+db.createCollection("streamings");
+
+// Inserindo na coleção
+db.streamings.insertOne(
+    {
+        "name": "Disney+",
+        "subscription_fee": 27.90,
+        "headquarters": "Los Angeles",
+        "founded": new Date("2019-11-12"),
+        "media": [
+            db.visual_media.findOne({"title": "Star Wars: Episódio V - O Império Contra-Ataca"})._id,
+            db.visual_media.findOne({"title": "Encanto"})._id,
+            db.visual_media.findOne({"title": "O Rei Leão"})._id,
+            db.visual_media.findOne({"title": "Homem de Ferro 3"})._id,
+            db.visual_media.findOne({"title": "A Bela Adormecida"})._id,
+            db.visual_media.findOne({"title": "The Mandalorian"})._id,
+            db.visual_media.findOne({"title": "Clone Wars"})._id,
+        ]
+    }
+);
+
+db.streamings.insertOne(
+    {
+        "name": "Netflix",
+        "headquarters": "Los Gatos",
+        "founded": new Date("1997-08-29"),
+        "media": [
+            db.visual_media.findOne({"title": "A Viagem de Chihiro"})._id,
+            db.visual_media.findOne({"title": "Karate Kid"})._id,
+            db.visual_media.findOne({"title": "O Som ao Redor"})._id,
+            db.visual_media.findOne({"title": "Ilha do Medo"})._id,
+            db.visual_media.findOne({"title": "De Volta para o Futuro"})._id,
+            db.visual_media.findOne({"title": "Breaking Bad"})._id,
+            db.visual_media.findOne({"title": "Avatar: A lenda de Aang"})._id,
+        ]
+    }
+);
+
+db.streamings.insertOne(
+    {
+        "name": "HBO Max",
+        "headquarters": "Burbank",
+        "founded": new Date("2020-05-27"),
+        "media": [
+            db.visual_media.findOne({"title": "Se Beber, Não Case!"})._id,
+            db.visual_media.findOne({"title": "A Freira"})._id,
+            db.visual_media.findOne({"title": "Matrix"})._id,
+            db.visual_media.findOne({"title": "Interestelar"})._id,
+            db.visual_media.findOne({"title": "Até o Último Homem"})._id,
+            db.visual_media.findOne({"title": "Game of Thrones"})._id,
+            db.visual_media.findOne({"title": "Supernatural"})._id,
+        ]
+    }
+);
+
+db.streamings.insertOne(
+    {
+        "name": "Prime Video",
+        "headquarters": "Seattle",
+        "founded": new Date("2006-09-07"),
+        "media": [
+            db.visual_media.findOne({"title": "O Senhor dos Anéis: A Sociedade do Anel"})._id,
+            db.visual_media.findOne({"title": "O Silêncio dos Inocentes"})._id,
+            db.visual_media.findOne({"title": "Esposa de Mentirinha"})._id,
+            db.visual_media.findOne({"title": "Como eu era antes de você"})._id,
+            db.visual_media.findOne({"title": "A Múmia"})._id,
+            db.visual_media.findOne({"title": "House, M.D."})._id,
+            db.visual_media.findOne({"title": "How I Met Your Mother"})._id,
+        ]
+    }
+);
+
+db.streamings.insertOne(
+    {
+        "name": "Globoplay",
+        "headquarters": "Rio de Janeiro",
+        "founded": new Date("2015-11-03"),
+        "media": [
+            db.visual_media.findOne({"title": "Tropa de Elite"})._id,
+            db.visual_media.findOne({"title": "As Vantagens De Ser Invisível"})._id,
+            db.visual_media.findOne({"title": "Cidade De Deus"})._id,
+            db.visual_media.findOne({"title": "Homem-Aranha"})._id,
+            db.visual_media.findOne({"title": "Top Gun - Asas Indomáveis"})._id,
+	        db.visual_media.findOne({"title": "A Grande Família"})._id,
+	        db.visual_media.findOne({"title": "Um Maluco no Pedaço"})._id,
+        ]
+    }
+);
+
+// Criando a coleção de inscrições
+db.createCollection("inscricoes");
+
+// Inserindo na coleção
+db.inscricoes.insertOne(
+    {
+        "name": "Netflix Básico",
+        "pacote": false,
+        "subscription_fee": 25.90,
+        "cancellation_fee": 0,
+        "duration": "mensal",
+        "screens": 1,
+        "platforms": [
+            db.streamings.findOne({"name": "Netflix"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "Netflix Padrão",
+        "pacote": false,
+        "subscription_fee": 39.90,
+        "cancellation_fee": 0,
+        "duration": "mensal",
+        "screens": 2,
+        "platforms": [
+            db.streamings.findOne({"name": "Netflix"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "Netflix Premium",
+        "pacote": false,
+        "subscription_fee": 55.90,
+        "cancellation_fee": 0,
+        "duration": "mensal",
+        "screens": 4,
+        "platforms": [
+            db.streamings.findOne({"name": "Netflix"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "Disney+ mensal",
+        "pacote": false,
+        "subscription_fee": 27.90,
+        "cancellation_fee": 0,
+        "duration": "mensal",
+        "screens": 4,
+        "platforms": [
+            db.streamings.findOne({"name": "Disney+"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "Disney+ anual",
+        "pacote": false,
+        "subscription_fee": 279.90,
+        "cancellation_fee": 0,
+        "duration": "anual",
+        "screens": 4,
+        "platforms": [
+            db.streamings.findOne({"name": "Disney+"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "HBO Max mensal",
+        "pacote": false,
+        "subscription_fee": 27.90,
+        "cancellation_fee": 0,
+        "duration": "mensal",
+        "screens": 3,
+        "platforms": [
+            db.streamings.findOne({"name": "HBO Max"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "HBO Max anual",
+        "pacote": false,
+        "subscription_fee": 239.90,
+        "cancellation_fee": 0,
+        "duration": "anual",
+        "screens": 3,
+        "platforms": [
+            db.streamings.findOne({"name": "HBO Max"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "Prime Video mensal",
+        "pacote": false,
+        "subscription_fee": 14.90,
+        "cancellation_fee": 0,
+        "duration": "mensal",
+        "screens": 3,
+        "platforms": [
+            db.streamings.findOne({"name": "Prime Video"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "Prime Video anual",
+        "pacote": false,
+        "subscription_fee": 119.0,
+        "cancellation_fee": 0,
+        "duration": "anual",
+        "screens": 3,
+        "platforms": [
+            db.streamings.findOne({"name": "Prime Video"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "Globoplay mensal",
+        "pacote": false,
+        "subscription_fee": 22.90,
+        "cancellation_fee": 0,
+        "duration": "mensal",
+        "screens": 5,
+        "platforms": [
+            db.streamings.findOne({"name": "Globoplay"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "Globoplay anual",
+        "pacote": false,
+        "subscription_fee": 238.80,
+        "cancellation_fee": 0,
+        "duration": "anual",
+        "screens": 5,
+        "platforms": [
+            db.streamings.findOne({"name": "Globoplay"})._id,
+        ]
+    }
+);
+
+
+db.inscricoes.insertOne(
+    {
+        "name": "Pacote Não falamos do Bruno",
+        "pacote": true,
+        "subscription_fee": 459.80,
+        "cancellation_fee": 45.0,
+        "duration": "anual",
+        "screens": 3,
+        "platforms": [
+            db.streamings.findOne({"name": "Globoplay"})._id,
+            db.streamings.findOne({"name": "Disney+"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "Pacote I have the high ground",
+        "pacote": true,
+        "subscription_fee": 72.90,
+        "cancellation_fee": 10.0,
+        "duration": "mensal",
+        "screens": 3,
+        "platforms": [
+            db.streamings.findOne({"name": "Netflix"})._id,
+            db.streamings.findOne({"name": "Prime Video"})._id,
+            db.streamings.findOne({"name": "Disney+"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "Pacote Beginning to Believe",
+        "pacote": true,
+        "subscription_fee": 400,
+        "cancellation_fee": 0,
+        "duration": "anual",
+        "screens": 3,
+        "platforms": [
+            db.streamings.findOne({"name": "HBO Max"})._id,
+            db.streamings.findOne({"name": "Globoplay"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "Pacote To Rule Them All",
+        "pacote": true,
+        "subscription_fee": 106.80,
+        "cancellation_fee": 0,
+        "duration": "mensal",
+        "screens": 3,
+        "platforms": [
+            db.streamings.findOne({"name": "HBO Max"})._id,
+            db.streamings.findOne({"name": "Globoplay"})._id,
+            db.streamings.findOne({"name": "Netflix"})._id,
+            db.streamings.findOne({"name": "Prime Video"})._id,
+            db.streamings.findOne({"name": "Disney+"})._id,
+        ]
+    }
+);
+
+db.inscricoes.insertOne(
+    {
+        "name": "Pacote Great Power",
+        "pacote": true,
+        "subscription_fee": 94.90,
+        "cancellation_fee": 0,
+        "duration": "mensal",
+        "screens": 3,
+        "platforms": [
+            db.streamings.findOne({"name": "HBO Max"})._id,
+            db.streamings.findOne({"name": "Globoplay"})._id,
+            db.streamings.findOne({"name": "Netflix"})._id,
+            db.streamings.findOne({"name": "Prime Video"})._id,
+        ]
+    }
+);
