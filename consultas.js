04f56360028075ea235f31415c159eb4196ecf8e
Bonna Borsoi <86304672+bonnaborsoi@users.noreply.github.com>
2022-10-10 21:23:50 -0300

    Update consultas.js

--- consultas.js
+++ consultas.js
@@ -7,5 +7,16 @@ db.inscricoes.find({platforms:{$size: 1}}).pretty();
 //MATCH: lista todas as inscrições que contêm combos
 db.inscricoes.aggregate([{$match : {combo: true}}]);
 
+//GTE: seleciona todos os filmes/séries que tem nota maior ou igual a 8.5 no IMDB
+//Mostra os selecionados com título e nota em ordem crescente
+db.visual_media.find({
+    imdb_score: {
+        $gte: 8.5
+    }
+}, {
+    title: 1,
+    imdb_score: 1
+}).sort({imdb_score : 1});
+
 //COUNT: conta a quantidade total de filmes/séries
 db.visual_media.count();
