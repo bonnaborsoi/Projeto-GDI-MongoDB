3a617e8099fe47c3a7e0eac012de58e58723b053
joaomarcelovalencachacon <86393443+joaomarcelovalencachacon@users.noreply.github.com>
2022-10-10 21:05:52 -0300

    COUNT

--- consultas.js
+++ consultas.js
@@ -1,4 +1,8 @@
 //FIND: retorna todos as plataformas de streaming que estão no banco de dados
 db.streamings.find().pretty(); 
+
 //SIZE: lista as inscrições com 1 plataforma de streaming
 db.inscricoes.find({platforms:{$size: 1}}).pretty();
+
+//COUNT: conta a quantidade total de filmes/séries
+db.visual_media.count();
