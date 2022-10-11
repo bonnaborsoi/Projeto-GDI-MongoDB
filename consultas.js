e9ced87b3f629b5d13ba49d00c2d8df593e5053b
Bonna Borsoi <86304672+bonnaborsoi@users.noreply.github.com>
2022-10-10 21:09:09 -0300

    Update consultas.js

--- consultas.js
+++ consultas.js
@@ -4,5 +4,8 @@ db.streamings.find().pretty();
 //SIZE: lista as inscrições com 1 plataforma de streaming
 db.inscricoes.find({platforms:{$size: 1}}).pretty();
 
+//MATCH: lista todas as inscrições que contêm combos
+db.inscricoes.aggregate([{$match : {combo: true}}]);
+
 //COUNT: conta a quantidade total de filmes/séries
 db.visual_media.count();
